983e27bafcf87d7d57ca5598d3f9ad8a7f26c4db
rkonesky <rosskon1023@gmail.com>
2022-01-22 10:55:34 -0600

    front end wip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { auth } from './services/firebase';
 import './App.css';
 
 // Import Components
@@ -5,10 +7,17 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => setUser(user));
+  }, []);
+  // In the above code, user is either null or a user with their details 
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header user={user} />
+      <Main user={user} />
     </div>
   );
 }
